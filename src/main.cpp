7a1d32fd4dd5fa63e655fe7982341b19b40decf1
Gustavo <s3898300@student.rmit.edu.au>
2026-05-16 12:15:50 +1000

    Differentiates light source from eye

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,12 @@ int main()
     std::string str = "";
     Scene scene = Scene();
 
-    for (int i = 1; i < 5; i++)
+    for (int i = 1; i < 2; i++)
     {
         scene.addObject(Sphere(Vec3(32 * i, 128, 10), 70));
     }
     Vec3 eye = Vec3(256, 256, -100);
+    Vec3 lightSource = Vec3(400, -200, -100);
 
     std::cout << "Ray tracer starting..." << std::endl;
     for (int i = 0; i < 256; i++)
@@ -33,14 +34,24 @@ int main()
             Vec3 pixel = Vec3(i, j, 0);
             Ray ray = Ray(eye, pixel - eye);
             HitRecord hitRecord = scene.sceneCollision(ray);
-            double brightness = (ray.direction.normalise() * -1) * hitRecord.normal;
+            // hitppoint from eye
+            //  calculate hitpoint - lightsoutce,
+            // gives me the direction the light is hitting the point at
+            Ray lightRay = Ray(lightSource, hitRecord.hitPoint - lightSource);
+            double brightness = (lightRay.direction.normalise() * -1) * hitRecord.normal;
 
             if (hitRecord.hit == true && brightness > 0)
             {
 
-                out << (int)(100 * brightness) << " ";
+                out << (int)(255 * brightness) << " ";
                 out << 0 << " ";
                 out << (int)(0) << "\t";
+            }
+            else if (hitRecord.hit == true)
+            {
+                out << 20 << " ";
+                out << 0 << " ";
+                out << 0 << "\t";
             }
 
             else
